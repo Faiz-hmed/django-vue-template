652d0cc623d5fe325998d60bd9c6f500fabf1bb9
faiz-hmed <faiz.ahmeds2000@gmail.com>
2020-05-25 00:56:25 +0530

    Random changes

--- vue.config.js
+++ vue.config.js
@@ -13,8 +13,7 @@ module.exports = {
     devServer: {
       proxy: {
         '/api*': {
-          // Forward frontend dev server request for /api to django dev server
-          target: 'http://localhost:8000/',
+          
         }
       }
     }
